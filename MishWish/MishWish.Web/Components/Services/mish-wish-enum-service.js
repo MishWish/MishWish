a2cd2ef2dddf77f030fef5212bc966a9d5de91bc
Nikhil1292 <nktambave@gmail.com>
2016-06-09 01:21:42 +0530

    WEB : #6 1. Added enum service

---
+++ MishWish/MishWish.Web/Components/Services/mish-wish-enum-service.js
@@ -0,0 +1,6 @@
+/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+angular.module('MishWishApp.EnumsService', ['ngResource'])
+    .factory('EnumsService', ['$resource', function ($resource) {
+        var allEnums = {};
+        return allEnums;
+    }]);
---
+++ MishWish/MishWish.Web/Components/Services/mish-wish-enum-service.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+
+angular.module('MishWishApp.EnumsService', ['ngResource'])
+
+    .factory('EnumsService', ['$resource', function ($resource) {
+
+        var allEnums = {
+
+
+        };
+
+        return allEnums;
+
+    }]);
